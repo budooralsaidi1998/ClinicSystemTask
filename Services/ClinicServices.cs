c8c2492afa0e1eef362e566ced9aa1e53fd231f6
budoor <budooralsaidi1998@gmail.com>
2024-12-05 15:21:26 +0400

    add services of clinic

--- Services/ClinicServices.cs
+++ Services/ClinicServices.cs
@@ -1,6 +1,39 @@
-﻿namespace ClinicSystem.Services
+﻿using ClinicSystem.models;
+using ClinicSystem.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicSystem.Services
 {
     public class ClinicServices
     {
+
+        private readonly ICliniRepo _clinicRepo;
+
+        public ClinicServices(ICliniRepo clinicRepo)
+        {
+            _clinicRepo = clinicRepo;
+        }
+
+        public List<Clinic> ViewClinicDetatils()
+        {
+
+            return _clinicRepo.ViewClinic().ToList();
+
+        }
+
+
+        public int  AddClinic(Clinic clinic)
+        {
+       
+           
+
+
+
+            return _clinicRepo.Add(clinic);
+        }
+
+
+       
+
     }
 }
