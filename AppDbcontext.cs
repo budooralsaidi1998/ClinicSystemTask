e3233db427923d744d6a72ff7d465b7bf85b28ad
budoor <budooralsaidi1998@gmail.com>
2024-12-05 14:37:50 +0400

    add consturct connection

---
+++ AppDbcontext.cs
@@ -0,0 +1,21 @@
+﻿using ClinicSystem.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicSystem
+{
+    public class AppDbcontext:DbContext
+    {
+
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+
+        }
+         
+        public DbSet<Clinic> clinics { get; set; }
+        public DbSet<Patient> patients { get; set; }
+
+        public DbSet<booking> bookings { get; set; }
+
+
+    }
+}
