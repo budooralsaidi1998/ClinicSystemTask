c9d574ca99c9b775d9dc1f606b2737046fce11c8
budoor <budooralsaidi1998@gmail.com>
2024-12-09 09:55:42 +0400

    add the method endpoint for addpatient

--- Controller/PatientController.cs
+++ Controller/PatientController.cs
@@ -1,11 +1,12 @@
-﻿using ClinicSystem.Services;
+﻿using ClinicSystem.models;
+using ClinicSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicSystem.Controller
 {
     [ApiController]
     [Route("api/[Controller]")]
-    public class PatientController
+    public class PatientController:ControllerBase
     {
         private readonly IPatientServices _PatientService;
 
@@ -13,5 +14,34 @@ namespace ClinicSystem.Controller
         {
             _PatientService = pa;
         }
+
+
+
+
+        [HttpPost]
+        public IActionResult AddPatient(string name, int age, string gender)
+        {
+            try
+            {
+
+                if (!Enum.TryParse<GenderType>(gender, true, out var genderEnum))
+                {
+                    return BadRequest("Invalid gender value. Please provide 'Male', 'Female', or 'Other'.");
+                }
+
+                string newPatient = _PatientService.AddPatient(new Patient
+                {
+                    pname = name,
+                    age = age,
+                    RGender = genderEnum
+                });
+
+                return Created(string.Empty, newPatient);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
-}   
+}
